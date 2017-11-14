765f69ef4b47462c8aab1d48dbb129a653042b83
Caroline Heymes <caroline.heymes@gmail.com>
2017-11-14 20:25:58 +0000

    reprise à Zero

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+    
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:fname, :lname, :name, :phone, :surname,:username, :email, :avatar]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [attributes])
+    devise_parameter_sanitizer.permit(:account_update, keys: [attributes])
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :team]
 
   def home
   end
+  
+  def team
+  end
+  
+  def create
+    redirect_to team.path
+  end
+  
 end
